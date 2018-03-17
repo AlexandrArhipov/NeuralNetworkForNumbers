c88a1f0eba1da26a99512bf1baed84cc2f9c9305
Александр <alexudalovvvvv@outlook.com>
2018-03-17 15:26:50 +0300

    Added JpegEncoder

---
+++ NeuralNetworkForNumbers/JpegEncoder.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Drawing;
+using System.IO;
+
+namespace NeuralNetworkForNumbers
+{
+    static class JpegEncoder
+    {
+        public static int[,] Encode(string path, int rows, int columns)
+        {
+            int[,] result = new int[rows, columns];
+            Bitmap bitmap = ConvertToBitmap(path);
+
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    result[i, j] = bitmap.GetPixel(i, j).B;
+
+            return result;
+        }
+
+        private static Bitmap ConvertToBitmap(string fileName)
+        {
+            Bitmap bitmap;
+            using (Stream bmpStream = File.Open(fileName, FileMode.Open))
+            {
+                Image image = Image.FromStream(bmpStream);
+                bitmap = new Bitmap(image);
+            }
+            return bitmap;
+        }
+    }
+}
--- NeuralNetworkForNumbers/Program.cs
+++ NeuralNetworkForNumbers/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace NeuralNetworkForNumbers
+﻿using System;
+
+namespace NeuralNetworkForNumbers
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            int[,] result = JpegEncoder.Encode(@"C:\Users\alexu\source\repos\NeuralNetworkForNumbers\NeuralNetworkForNumbers\TrainingSet\0\img_1.jpg", 28, 28);
+
+            for (int i = 0; i < 28; i++)
+            {
+                for (int j = 0; j < 28; j++)
+                {
+                    Console.Write(String.Format("{0,4:0.0}", result[i,j] + " "));
+
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadLine();
         }
     }
 }
